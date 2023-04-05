624c336f41db4f77c9f429361f1738f2d192a2a2
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2023-04-05 16:38:33 +0600

    update commenting

--- app/Controllers/Form.php
+++ app/Controllers/Form.php
@@ -11,12 +11,14 @@ class Form extends BaseController
 
     public function __construct()
     {
-        $this->session = \Config\Services::session();
+        $this->session = \Config\Services::session(); // Initialize session
     }
+
     public function index()
     {
-    return view('form');
+        return view('form'); // Load form view
     }
+
     public function stor()
     {
         $data = [];
@@ -24,55 +26,53 @@ class Form extends BaseController
             'email' => 'required|valid_email',
             'password' => 'required|min_length[6]'
         ];
-    
+
+        // Check if form is submitted with POST method
         if ($this->request->getMethod() == 'post') {
             if ($this->validate($rules)) {
                 // Validation passed, insert data into the database
                 $email = $this->request->getPost('email');
                 $password = $this->request->getPost('password');
                 $token = bin2hex(random_bytes(32));
-    
+
                 $data = [
                     'email' => $email,
                     'activation_token' =>  $token,
                     'password' => password_hash($password, PASSWORD_DEFAULT),
-
                 ];
-    
-                $db = \Config\Database::connect();
-                $builder = $db->table('user');
-                $builder->insert($data);
 
-                
+                $db = \Config\Database::connect(); // Get database connection
+                $builder = $db->table('user'); // Get table builder
+                $builder->insert($data); // Insert data into the user table
 
                 $to = $email;
                 $subject = 'Confirm your registration';
-                $message = 'Thank you for registering. Please click the following link to activate your account: ' . base_url() . '/activate/' . $token;
-                
-                $email = \Config\Services::email();
+                $message = '
+                <p>Thank you for registering. Please click the following button to activate your account:</p>
+                <a href="' . base_url() . '/activate/' . $token . '" style="display:inline-block;padding:12px 24px;background-color:#007bff;color:#fff;text-decoration:none;">Activate Account</a>
+              ';
+
+                $email = \Config\Services::email(); // Get email service
                 $email->setTo($to);
                 $email->setFrom('noreply@example.com', 'My Website');
                 $email->setSubject($subject);
                 $email->setMessage($message);
-                $email->send();
+                $email->send(); // Send activation email to user
 
-    
                 // Set success message
                 $session = \Config\Services::session();
                 $session->setFlashdata('success', 'registation  successfully! Please activate this account ');
-                return redirect()->to('login');
+                return redirect()->to('login'); // Redirect to login page
             } else {
                 // Validation failed, store errors in the $data array
                 $data['validation'] = $this->validator;
             }
         }
-    
+
         // Get the success message (if any) from the session
         $session = \Config\Services::session();
         $data['success'] = $session->getFlashdata('success');
-    
-        return view('form', $data);
+
+        return view('form', $data); // Load form view with data
     }
-    
-    
 }
--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -4,59 +4,67 @@ namespace App\Controllers;
 
 class LoginController extends BaseController
 {
+    // Displays the login page
     public function index()
     {
         return view('login');
     }
 
+    // Handles the login form submission
     public function login()
     {
-        $data = [];
+        // Define the validation rules for the form
         $rules = [
             'email' => 'required|valid_email',
             'password' => 'required|min_length[6]'
         ];
-    
+
+        // Check if the form was submitted via POST request
         if ($this->request->getMethod() == 'post') {
+            // Validate the form data against the defined rules
             if ($this->validate($rules)) {
                 $email = $this->request->getPost('email');
                 $password = $this->request->getPost('password');
-    
+
+                // Connect to the database and retrieve the user by email
                 $db = \Config\Database::connect();
                 $builder = $db->table('user');
                 $user = $builder->where('email', $email)->get()->getRow();
-    
+
+                // If a user with the given email exists, check the password
                 if ($user) {
                     if (password_verify($password, $user->password)) {
-                        
-
-
+                        // If the user's account is verified, log them in and set a success message
                         if ($user->stutus == 1) {
                             $session = \Config\Services::session();
                             $session->set('user_id', $user->id);
-    
+
                             $session->setFlashdata('success', 'Logged in successfully!');
                             return view('login');
                         } else {
+                            // If the user's account is not verified, set an error message
                             $data['error'] = 'Your account is not verified. Please check your email for a verification link.';
                         }
                     } else {
+                        // If the password is incorrect, set an error message
                         $data['error'] = 'Invalid email or password';
                     }
                 } else {
+                    // If no user with the given email exists, set an error message
                     $data['error'] = 'Invalid email or password';
                 }
             } else {
+                // If the form validation fails, store the errors in the $data array
                 $data['validation'] = $this->validator;
             }
         }
-    
+
+        // Get the success message (if any) and error message from the session
         $session = \Config\Services::session();
         $data['success'] = $session->getFlashdata('success');
         $data['error'] = isset($data['error']) ? $data['error'] : $session->getFlashdata('error');
-    
+
+        // Display the login page with the $data array passed to it
         return view('login', $data);
     }
-    
-    
 }
